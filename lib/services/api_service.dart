648cbbbd988f810a0632ef3babbc7075ac3b6797
MariamMoh18 <mariammohamed18604@gmail.com>
2026-05-16 11:10:33 +0300

    Switch backend port to 8000, add cleartext traffic flag to Android manifest

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -11,8 +11,8 @@ class ApiService {
   );
 
   // Default to loopback and use adb reverse for Android physical devices.
-  static const String _deviceUrl = 'http://192.168.1.12:5000';
-  static const String _webUrl = 'http://localhost:5000';
+  static const String _deviceUrl = 'http://192.168.1.12:8000';
+  static const String _webUrl = 'http://localhost:8000';
 
   static String get baseUrl {
     if (_configuredApiBase.isNotEmpty) {
